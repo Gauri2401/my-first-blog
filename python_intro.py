9b58346daeafd0e6b391a49589cdd943fc6be36f
gauri <gauri@gmail.com>
2024-04-07 01:44:33 +0530

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,20 @@
+print("hello")
+if (3>2):
+    print("hii")
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+
+hi("Ola")
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
